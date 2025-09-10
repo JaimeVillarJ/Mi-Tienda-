78127a4abdd73e6451d1a65696d868f6da3a7b6e
JaimeVillarJ <jaimevvvjjj@gmail.com>
2025-09-09 21:23:27 -0500

    add shoppingCar to promotions

--- src/components/Promotions.tsx
+++ src/components/Promotions.tsx
@@ -3,43 +3,50 @@ import { useEffect, useState } from "react";
 import type { products } from "../types/types";
 import "../styles/products.css";
 
-export const Promotions = () => {
+export const Promotions = ({
+  addToCart,
+}: {
+  addToCart: (product: products) => void;
+}) => {
+  const [products, setProducts] = useState<products[]>([]);
+  const [chargeProducts, setChargeProducts] = useState(true);
 
-    const [products, setProducts] = useState<products[]>([]);
-    const [chargeProducts, setChargeProducts] = useState(true);
+  const API_LINK = import.meta.env.VITE_API_LINK;
 
-    const API_LINK = import.meta.env.VITE_API_LINK;
-    useEffect(() => {
-        axios
-        .get(`${ API_LINK }/api/products/promotions`)
-        .then((response) => {
-            setProducts(response.data);
-            setChargeProducts(false);
-        })
-        .catch(() => {
-            alert("❌ Error al cargar los productos");
-            setChargeProducts(false);
-        });
-    }, []);
+  useEffect(() => {
+    axios
+      .get<products[]>(`${API_LINK}/api/products/promotions`)
+      .then((response) => {
+        setProducts(response.data);
+        setChargeProducts(false);
+      })
+      .catch(() => {
+        alert("❌ Error al cargar los productos");
+        setChargeProducts(false);
+      });
+  }, []);
 
-    return (
-        <div>
-            <h1 className="news-title">DESCUENTOS QUE NO ENCONTRARAS EN OTRO LUGAR</h1>
-            { chargeProducts ? (
-            <p className="cargando">Cargando productos...</p>
-            ) : (
-            <div className="products-grid">
-                {products.map((item) => (
-                <div className="catalog-container" key={ item.id }>
-                    <h2>{ item.name }</h2>
-                    <img src={ item.link } alt={ item.name } />
-                    <p>{ item.price }</p>
-                    <button className="add">Agregar</button>
-                </div>
-                ))}
+  return (
+    <div>
+      <h1 className="news-title">
+        DESCUENTOS QUE NO ENCONTRARAS EN OTRO LUGAR
+      </h1>
+      {chargeProducts ? (
+        <p className="cargando">Cargando productos...</p>
+      ) : (
+        <div className="products-grid">
+          {products.map((item) => (
+            <div className="catalog-container" key={item.id}>
+              <h2>{item.name}</h2>
+              <img src={item.link} alt={item.name} />
+              <p>{item.price}</p>
+              <button className="add" onClick={() => addToCart(item)}>
+                Agregar
+              </button>
             </div>
-            )}
+          ))}
         </div>
-    );
-
-}
+      )}
+    </div>
+  );
+};
